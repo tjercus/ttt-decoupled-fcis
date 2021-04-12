b6ec8e55de853aa5afc4bcf534f336dd3e5021c1
tvalentijn <tjerk.valentijn@ebpi.nl>
2021-04-12 14:31:22 +0200

    unadded files

---
+++ src/core.test.js
@@ -0,0 +1,44 @@
+import { initialBoard, makeBoardBasedOnMove, validateMove } from "./core";
+
+describe("core", () => {
+  describe("validateMove", () => {
+    it("should validate true on unused tile", () => {
+      const board = initialBoard;
+      const move = {
+        player: "x",
+        tile: {
+          colIndex: 1,
+          rowIndex: 1,
+          value: "_",
+        },
+      };
+      expect(validateMove(board, move)).toBe(true);
+    });
+    it("should validate false when moving on a used tile", () => {
+      const move = {
+        player: "x",
+        tile: {
+          colIndex: 1,
+          rowIndex: 1,
+          value: "_",
+        },
+      };
+      const board = makeBoardBasedOnMove(initialBoard, move);
+      expect(validateMove(board, move)).toBe(false);
+    });
+  });
+  describe("makeBoardBasedOnMove", () => {
+    it("should make a board based on a move", () => {
+      const move = {
+        player: "x",
+        tile: {
+          colIndex: 1,
+          rowIndex: 1,
+          value: "_",
+        },
+      };
+      const board = makeBoardBasedOnMove(initialBoard, move);
+      expect(board[1][1]).toEqual({ colIndex: 1, rowIndex: 1, value: "x" });
+    });
+  });
+});
